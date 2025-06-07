c79d02e9c3a055392e0c02f0f13e9798747165f9
Myriam <myriamalbustany@gmail.com>
2025-06-07 23:15:31 +0300

    added configs

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.title = "Knight's Tour"
+    t.version = "11.5"
+    t.window.width = 550
+    t.window.height = 650
+    t.window.resizable = true
+    t.console = true
+end 
